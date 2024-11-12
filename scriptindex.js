2e81d8769f3d7f86ee16a497e7e22cac8778ca3b
AlcanceServer <alcanceserver2024@gmail.com>
2024-11-12 14:41:13 +0000

    animações

--- scriptindex.js
+++ scriptindex.js
@@ -4,15 +4,29 @@ var nome = document.getElementById("nome") //O valor no campo do Nome
 var pass = document.getElementById("pass") //O valor no campo da Palavra-Passe
 
 
+nome.addEventListener("click",(event)=>{
+    document.getElementById("btn_entrar").className="before_login";
+    document.getElementById("btn_entrar").value = 'Entrar';
+})
+
 //Função que verifica se as entradas estão certas ou não
 document.getElementById("btn_entrar").addEventListener("click",(event) =>{
     for (let i = 0; i < entradas[0].length; i++) {
         if(nome.value == entradas[0][i]){
             console.log("Username exists! "+ entradas[0][i])
             if(pass.value == entradas[1][i]){
+                document.getElementById("btn_entrar").className="after_login";
+                document.getElementById("btn_entrar").value = '✔';
                 console.log("Login efetuado com sucesso!");
-                window.open("server.html","_self");
+                setTimeout(() => {
+                    window.open("server.html","_self");
+                }, 1000);
             }
+        }
+        else{
+            document.getElementById("btn_entrar").className="error_login";
+            document.getElementById("btn_entrar").value = '❌';
+            console.log("Login efetuado com erro!");
         }
     }
 })
