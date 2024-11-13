17fd1779bed7595185689368b994e0a827d3ec5c
AlcanceServer <alcanceserver2024@gmail.com>
2024-11-13 00:29:42 +0000

    Melhorias

--- scriptindex.js
+++ scriptindex.js
@@ -1,4 +1,42 @@
-var entradas = [/*Nomes de Utilizador*/["Agata", "Carlos"], /*Palavras-Passe*/["1234Aga", "1234Car"]];
+var entradas
+
+async function decryptFile() {
+    // Retrieve encrypted content, key, and iv from input fields
+    const encryptedBase64 = 'tQYHe6D5pLbMjwDznBfG56DT/yh4Td44fLtg3egJ9jtJcm/m8PXYxQe7bNtpida32jWoMpX0LvzusiuB';
+    const keyBase64 = 'O2ZYjhUsmZAL3NfXe8Dg0OXc+SYlawu8s8365Tc7nws=';
+    const ivBase64 = 'XEyXn6tXhzJz6tXP';
+
+    // Convert Base64 to Uint8Array
+    const encryptedData = new Uint8Array(atob(encryptedBase64).split('').map(char => char.charCodeAt(0)));
+    const keyData = new Uint8Array(atob(keyBase64).split('').map(char => char.charCodeAt(0)));
+    const ivData = new Uint8Array(atob(ivBase64).split('').map(char => char.charCodeAt(0)));
+
+    // Import the key from Base64
+    const key = await crypto.subtle.importKey(
+      "raw",
+      keyData,
+      { name: "AES-GCM" },
+      true,
+      ["decrypt"]
+    );
+
+    // Decrypt the data using AES-GCM
+    try {
+      const decryptedData = await crypto.subtle.decrypt(
+        { name: "AES-GCM", iv: ivData },
+        key,
+        encryptedData
+      );
+
+      const decryptedText = new TextDecoder().decode(decryptedData);
+      entradas = JSON.parse(decryptedText)
+
+    }catch(error){
+        console.log("ERRO")
+    }
+  }
+
+  decryptFile()
 
 var nome = document.getElementById("nome"); //Campo Nome
 var pass = document.getElementById("pass"); //Campo Palavra-Passe
@@ -15,6 +53,15 @@ document.addEventListener("click", (event) => {
     }
 });
 
+document.getElementById("togglePassword").addEventListener("click", function () {
+    const passwordField = document.getElementById("pass");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+
+    // Toggle the eye icon's open/closed state
+    this.classList.toggle("eyeClosed");
+});
+
 // Função que verifica se as entradas estão corretas
 btnEntrar.addEventListener("click", () => {
     let foundUser = false;
@@ -29,7 +76,8 @@ btnEntrar.addEventListener("click", () => {
                 // Login bem-sucedido
                 btnEntrar.className = "after_login";
                 btnEntrar.value = '✔';
-
+                sessionStorage.setItem("Utilizador", nome.value);
+                sessionStorage.setItem("Success", "true");
                 setTimeout(() => {
                     window.open("server.html", "_self");
                 }, 1000);
---
+++ scriptsv.js
@@ -0,0 +1,3 @@
+if (sessionStorage.getItem("Success") !== "true") {
+    window.open("index.html","_self");
+}
