f5a5d5dd5904ed3214ad88fe7f18b346b672daa4
AlcanceServer <alcanceserver2024@gmail.com>
2024-11-15 23:28:06 +0000

    navBar

--- scriptsv.js
+++ scriptsv.js
@@ -2,4 +2,19 @@ if (sessionStorage.getItem("Success") !== "true") {
     window.open("index.html","_self");
 }
 
+var navBar = document.getElementById("nav_bar")
+var burguer_nav = document.getElementById("burguer_nav")
+
 document.getElementById("welcome_user").innerText = sessionStorage.getItem("Utilizador");
+
+document.getElementById("close_nav").addEventListener("click",(event)=>{
+    navBar.className = "nav_bar_closed";
+    burguer_nav.style.opacity = 1;
+    burguer_nav.style.scale = 1;
+})
+
+burguer_nav.addEventListener("click",()=>{
+    navBar.className = "nav_bar_open";
+    burguer_nav.style.opacity = 0;
+    burguer_nav.style.scale = 0;
+})
