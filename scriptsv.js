47946f04ef8053b2849acd9b77cd74182113cf55
AlcanceServer <alcanceserver2024@gmail.com>
2024-11-17 18:57:03 +0000

    Menu

--- scriptsv.js
+++ scriptsv.js
@@ -9,12 +9,18 @@ document.getElementById("welcome_user").innerText = sessionStorage.getItem("Util
 
 document.getElementById("close_nav").addEventListener("click",(event)=>{
     navBar.className = "nav_bar_closed";
-    burguer_nav.style.opacity = 1;
+    document.getElementById("action_tab").style.marginLeft = "20px";
     burguer_nav.style.scale = 1;
 })
 
 burguer_nav.addEventListener("click",()=>{
     navBar.className = "nav_bar_open";
-    burguer_nav.style.opacity = 0;
+    document.getElementById("action_tab").style.marginLeft = "220px";
     burguer_nav.style.scale = 0;
 })
+
+for(let i = 0;i<=(navBar.querySelectorAll("p").length-1);i++){
+    navBar.querySelectorAll("p")[i].addEventListener("click",()=>{
+        document.getElementById("action_tab").innerHTML = document.querySelectorAll(".actions")[i].innerHTML;
+    })
+}
